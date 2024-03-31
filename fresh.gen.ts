fceb7a5b0de0292ea1d041e3e7b86e7ea32bc751
tako0614 <shoutatomiyama0614@gmail.com>
2024-04-01 01:37:27 +0900

    データベース構造

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,25 +2,19 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $_404 from "./routes/_404.tsx";
-import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
+import * as $_middlewear from "./routes/_middlewear.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
-import * as $Counter from "./islands/Counter.tsx";
+
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/_404.tsx": $_404,
-    "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
+    "./routes/_middlewear.ts": $_middlewear,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
   },
-  islands: {
-    "./islands/Counter.tsx": $Counter,
-  },
+  islands: {},
   baseUrl: import.meta.url,
 } satisfies Manifest;
 
--- main.ts
+++ main.ts
@@ -3,11 +3,20 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
-
+import { load } from "https://deno.land/x/dotenv/mod.ts";
+const env = await load();
 import "$std/dotenv/load.ts";
-
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
-
+import mongoose from "mongoose";
+import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
+/**connect mongoDB */
+const env = await load();
+const url = env["MONGO_URL"];
+await mongoose.connect(url).then(() => {
+  console.log("mongo DB 接続");
+}).catch((err) => {
+  console.log(err);
+});
 await start(manifest, config);
